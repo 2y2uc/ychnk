9fe52bc98ce4200a3aa79ad9f53514377a0b58db
ychnk <wxb2ar@gmail.com>
2026-04-15 20:56:55 +0900

    game 1

---
+++ UnityProject/Assets/PlayerController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+
+    void Update()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (moveInput.magnitude > 0)
+        {
+
+            myAnimator.SetBool("move", true);
+        }
+        else {
+            myAnimator.SetBool("move", false);
+        }
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene("PlayScene_" + collision.name); // DOOR 2
+                                                                   //
+        }
+    }
+
+}
+
